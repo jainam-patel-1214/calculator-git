5c1f915d7ede2e21c38c2048c0e8ca86cd446bc9
Jainem Patel <jainempatel@Jainams-MacBook-Air.local>
2025-11-18 15:20:34 +0530

    add and sub

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ func main() {
 	a := 10
 	b := 20
 	fmt.Println(add(a, b))
+	fmt.Println(sub(a, b))
 }
 func add(x int, y int) int {
 	return x + y
+}
+func sub(x int, y int) int {
+	return x - y
 }
