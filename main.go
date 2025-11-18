5a406945928bb24c5450f0ea3b4a88169c9974f2
Jainem Patel <jainempatel@Jainams-MacBook-Air.local>
2025-11-18 15:22:49 +0530

    Revert "wrong delete"
    This reverts commit 30e4a305485089df64a543642f5dbfed4e8f8495.

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ func main() {
 	fmt.Println(add(a, b))
 	fmt.Println(sub(a, b))
 	fmt.Println(mul(a, b))
-	fmt.Println(div(a, b))
 }
 func add(x int, y int) int {
 	return x + y
@@ -18,8 +17,5 @@ func sub(x int, y int) int {
 	return x - y
 }
 func mul(x int, y int) int {
-	return y * x
-}
-func div(x int, y int) int {
 	return y * x
 }
